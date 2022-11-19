1db28ed42912d91350a6f4ea35d7a0a68465b0f2
tatsuji_ho <longer271271@gmail.com>
2022-11-19 17:15:56 +0900

    フィールドとメソッドの間に改行追加

--- src/domain/model/user/email.py
+++ src/domain/model/user/email.py
@@ -4,6 +4,7 @@ from email_validator import validate_email, EmailNotValidError
 @dataclasses.dataclass(frozen=True)
 class Email:
     body: str
+    
     def __init__(self, body: str) -> None:
         try:
             validation = validate_email(body, check_deliverability=True)
