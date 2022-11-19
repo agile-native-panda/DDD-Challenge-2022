af85d28c17d2b4ab0b86cb19fc938ea8305a4989
tatsuji_ho <longer271271@gmail.com>
2022-11-19 17:06:09 +0900

    Emailの値オブジェクト（クラス）をバリデーション含めて実装

--- src/domain/model/user/email.py
+++ src/domain/model/user/email.py
@@ -1,3 +1,12 @@
+import dataclasses
+from email_validator import validate_email, EmailNotValidError
+
+@dataclasses.dataclass(frozen=True)
 class Email:
-    def __init__(self) -> None:
-        pass
+    body: str
+    def __init__(self, body: str) -> None:
+        try:
+            validation = validate_email(body, check_deliverability=True)
+            self.body = validation.email
+        except EmailNotValidError as e:
+            print(str(e))
