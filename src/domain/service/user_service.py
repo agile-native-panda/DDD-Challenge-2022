0fad8f4528fe9f61f2fc92a70fa4b243cf7dd299
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2022-11-20 20:40:36 +0900

    UserServiceにemailの重複確認を追加

--- src/domain/service/user_service.py
+++ src/domain/service/user_service.py
@@ -1,6 +1,10 @@
-from ..model import User
+from ..model import User, UserRepository
 
 
 class UserService:
     def IsDuplicated(self, user: User) -> bool:
-        pass
+        user_repository = UserRepository()
+        if user_repository.get_user_by_email(user):
+            raise ValueError("This email is already exist.")
+        else:
+            user_repository.create(user)
