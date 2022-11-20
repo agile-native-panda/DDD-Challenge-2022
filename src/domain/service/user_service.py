f29439c49da0bff1f240c3c484d2c5a7033d101f
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2022-11-20 20:42:47 +0900

    返り値が間違っていたので修正

--- src/domain/service/user_service.py
+++ src/domain/service/user_service.py
@@ -5,6 +5,6 @@ class UserService:
     def IsDuplicated(self, user: User) -> bool:
         user_repository = UserRepository()
         if user_repository.get_user_by_email(user):
-            raise ValueError("This email is already exist.")
+            return True
         else:
-            user_repository.create(user)
+            return False
