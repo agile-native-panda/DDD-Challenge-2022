e001fce0ebb82a3b07dcf26dc52d9bb62bb39416
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2022-11-20 22:17:07 +0900

    メソッドをスネークケースに変更

--- src/domain/service/user_service.py
+++ src/domain/service/user_service.py
@@ -2,7 +2,7 @@ from ..model import User, UserRepository
 
 
 class UserService:
-    def IsDuplicated(self, user: User) -> bool:
+    def is_duplicated(self, user: User) -> bool:
         user_repository = UserRepository()
         if user_repository.get_user_by_email(user):
             return True
