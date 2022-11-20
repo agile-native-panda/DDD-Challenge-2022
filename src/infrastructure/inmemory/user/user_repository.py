a378bd2365d085ba59137f47225ab967ef3fbc2e
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2022-11-20 23:06:30 +0900

    get_user_by_emailを実装

--- src/infrastructure/inmemory/user/user_repository.py
+++ src/infrastructure/inmemory/user/user_repository.py
@@ -1,13 +1,25 @@
-from src.domain.model.user import UserRepository, User
+from src.domain.model.user import UserRepository, User, Email
+from src.domain.service import UserService
+from typing import Optional
 
 
 class InMemoryUserRepository(UserRepository):
+    user_service = UserService()
+
     def __init__(self) -> None:
         self.store: list[User] = []
 
     def create(self, user: User) -> None:
-        # TODO: ここにemailの確認
-        self.save(user)
+        if self.user_service.is_duplicated(user):
+            raise ValueError("This email is already used.")
+        else:
+            self.save(user)
 
     def save(self, user: User) -> None:
         self.store += user
+
+    def get_user_by_email(self, email: Email) -> Optional[User]:
+        for user in self.store:
+            if email == user.email:
+                return user
+        return None
